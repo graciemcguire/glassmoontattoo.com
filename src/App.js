3d35f5262221fcb603ec580cd30e9c7b552c967b
Gracie <m.graciemcguire@gmail.com>
2019-07-02 17:41:40 -0400

    containers

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MainContainer from './Containers/MainContainer'
+import ArtistContainer from './Containers/ArtistContainer'
 
 function App() {
   return (
     <div className="App">
       <MainContainer />
+      <ArtistContainer />
     </div>
   );
 }
---
+++ src/Containers/ArtistContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class ArtistContainer extends Component {
+
+  render() {
+    return (
+      <div>
+      here's some artists
+      </div>
+    )
+  }
+}
