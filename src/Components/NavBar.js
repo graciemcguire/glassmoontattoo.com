1c7d001031cfb6b0ecb81a49543ad4006f2d681f
Gracie <m.graciemcguire@gmail.com>
2019-08-06 13:10:11 -0400

    test

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,8 +5,10 @@ export default class NavBar extends Component {
   render() {
     return (
       <div className='nav'>
-        <h1 style={{width:'100%'}}>glass moon tattoo</h1>
-        <h2>denver</h2>
+        <h1 className='logo'>glass moon tattoo</h1>
+        <div className='details'>
+          <h2>denver</h2>
+        </div>
       </div>
     )
   }
