0b0296968a988623e814aa0c5cb94ded8b44732d
github username <github email address>
2019-08-18 18:50:49 -0400

    list boi

--- src/Containers/MainContainer.js
+++ src/Containers/MainContainer.js
@@ -14,6 +14,13 @@ export default class MainContainer extends Component {
           <h3>contact info</h3>
           <h3>form?</h3>
           <h3>hours</h3>
+          <h1>style the whole thing</h1>
+          <h4>then...</h4>
+          <p>artist pages</p>
+          <p>instagram</p>
+          <p>more styling</p>
+          <p>probably make a backend</p>
+          <p>store client info, figure out uploading IDs</p>
         </div>
       </div>
     )
