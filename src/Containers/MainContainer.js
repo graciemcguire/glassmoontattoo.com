84fa9294db6d3a9a81690624a9aa7da3302b1c0d
Gracie <m.graciemcguire@gmail.com>
2019-08-19 10:52:48 -0400

    list

--- src/Containers/MainContainer.js
+++ src/Containers/MainContainer.js
@@ -9,12 +9,12 @@ export default class MainContainer extends Component {
 
         <div className="component">
           <h1>full site coming soon k</h1>
-          <h3>carousel</h3>
-          <h3>location</h3>
-          <h3>contact info</h3>
-          <h3>form?</h3>
-          <h3>hours</h3>
-          <h1>style the whole thing</h1>
+          <h3>carousel - ananas</h3>
+          <h3>location - g</h3>
+          <h3>contact info - g</h3>
+          <h3>form? - g</h3>
+          <h3>hours - g</h3>
+          <h1>style the whole thing - both?</h1>
           <h4>then...</h4>
           <p>artist pages</p>
           <p>instagram</p>
