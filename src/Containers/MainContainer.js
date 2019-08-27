a74ecfa3b2d4e88e53f13ba75868fad8aa741064
Gracie <m.graciemcguire@gmail.com>
2019-08-27 18:06:50 -0400

    align links

--- src/Containers/MainContainer.js
+++ src/Containers/MainContainer.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { withRouter } from 'react-router';
 import { Link } from 'react-router-dom'
 import logo from '../logo.png'
-import '../stylesheets/nav.scss'
+// import '../stylesheets/nav.scss'
 import '../stylesheets/main.scss'
 
 class MainContainer extends Component {
