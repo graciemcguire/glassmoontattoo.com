5e449df0283d03eec5dd15467b038eeae8ffb032
github username <github email address>
2019-08-27 19:36:43 -0400

    test git

--- src/Containers/MainContainer.js
+++ src/Containers/MainContainer.js
@@ -12,10 +12,10 @@ class MainContainer extends Component {
       <div className="main-container">
         <img className='logo-main' src={logo} alt='glass moon tattoo'/>
         <div className='nav-links'>
-          <Link className='nav-link' to={ '/' }>| HOME |</Link>
-          <Link className='nav-link' to={ '/about' }>| ABOUT |</Link>
-          <Link className='nav-link' to={ '/artists' }>| ARTISTS |</Link>
-          <Link className='nav-link' to={ '/contact' }>| CONTACT |</Link>
+          <Link className='nav-link' to={ '/' }> HOME </Link> |
+          <Link className='nav-link' to={ '/about' }> ABOUT </Link> |
+          <Link className='nav-link' to={ '/artists' }> ARTISTS </Link> |
+          <Link className='nav-link' to={ '/contact' }> CONTACT </Link>
         </div>
       </div>
     )
