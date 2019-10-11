b6bb06861720354a1a37e91e8d99bf50f4f402b5
Gracie <m.graciemcguire@gmail.com>
2019-10-11 12:20:45 -0400

    remove notes

--- src/Containers/MainContainer.js
+++ src/Containers/MainContainer.js
@@ -22,20 +22,3 @@ class MainContainer extends Component {
 }
 
 export default withRouter(MainContainer)
-
-//
-// <div className="component">
-//   <h1>full site coming soon k</h1>
-//   <h3>carousel - ananas</h3>no c
-//   <h3>location - g</h3>
-//   <h3>contact info - g</h3>
-//   <h3>form? - g</h3>
-//   <h3>hours - g</h3>
-//   <h1>style the whole thing - both?</h1>
-//   <h4>then...</h4>
-//   <p>artist pages</p>
-//   <p>instagram</p>
-//   <p>more styling</p>
-//   <p>probably make a backend</p>
-//   <p>store client info, figure out uploading IDs</p>
-// </div>
