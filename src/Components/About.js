802ab4d6649f5bcfc6ea93965bfd91632c4d801e
Gracie <m.graciemcguire@gmail.com>
2019-10-29 15:23:05 -0400

    restart

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,7 +6,22 @@ export default class About extends Component {
     return(
       <Fragment >
         <NavBar />
-        <h1>ABOUT</h1>
+        <div className='about-section'>
+          <img src='../images/about.jpg'/>
+          <h3>ABOUT</h3>
+          <p>De possumus arbitrantur o ex si velit elit ipsum. Noster voluptate ut
+            imitarentur ita id aute mentitum. Legam proident sempiternum, iis ullamco
+            coniunctione ea excepteur se cillum voluptate, magna id appellat in o et amet
+            ingeniis, ab export pariatur sempiternum, legam nam vidisse si nescius legam
+            velit quamquam tamen.Et fore eruditionem ex excepteur cillum ex appellat
+            coniunctione. Quo esse transferrem, ea si reprehenderit, admodum do multos, iis
+            id tamen culpa quid, pariatur cillum est offendit reprehenderit ab commodo quid
+            legam excepteur multos, incurreret enim iis arbitror comprehenderit, fore
+            voluptate an comprehenderit. Sint excepteur e praetermissum, quis cohaerescant
+            singulis export laborum, elit probant cohaerescant, pariatur ita sint. Deserunt
+            eruditionem o constias ab nam noster mentitum litteris, elit probant te
+            relinqueret, iudicem ipsum probant quamquam.    </p>
+        </div>
       </ Fragment>
     )
   }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import '../stylesheets/nav.scss'
+// import '../stylesheets/nav.scss'
 import { withRouter } from 'react-router';
 import { Link } from 'react-router-dom'
 
