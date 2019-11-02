1e0bb8f2496352afae58d078823d0eab543e3c90
github username <github email address>
2019-11-02 12:07:14 -0400

    hashrouter

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter} from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>
+  </Router>
   ,
   document.getElementById('root'));
 
