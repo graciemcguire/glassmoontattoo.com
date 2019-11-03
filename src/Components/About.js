9f1908e6c08a0100e88eda6c0f535f4429bc70c3
github username <github email address>
2019-11-02 20:52:44 -0400

    adds new artist photos

--- src/Components/About.js
+++ src/Components/About.js
@@ -11,18 +11,7 @@ export default class About extends Component {
         <div className='about-section'>
           <img className='about-image' src={`${process.env.PUBLIC_URL}/images/about.jpg`} alt='glassmoontattoo'/>
           <h3>ABOUT</h3>
-          <p className='about-text'>De possumus arbitrantur o ex si velit elit ipsum. Noster voluptate ut
-            imitarentur ita id aute mentitum. Legam proident sempiternum, iis ullamco
-            coniunctione ea excepteur se cillum voluptate, magna id appellat in o et amet
-            ingeniis, ab export pariatur sempiternum, legam nam vidisse si nescius legam
-            velit quamquam tamen.Et fore eruditionem ex excepteur cillum ex appellat
-            coniunctione. Quo esse transferrem, ea si reprehenderit, admodum do multos, iis
-            id tamen culpa quid, pariatur cillum est offendit reprehenderit ab commodo quid
-            legam excepteur multos, incurreret enim iis arbitror comprehenderit, fore
-            voluptate an comprehenderit. Sint excepteur e praetermissum, quis cohaerescant
-            singulis export laborum, elit probant cohaerescant, pariatur ita sint. Deserunt
-            eruditionem o constias ab nam noster mentitum litteris, elit probant te
-            relinqueret, iudicem ipsum probant quamquam. </p>
+          <p className='about-text'>Glass Moon Tattoo is a women owned and operated private tattoo studio in Denver, Colorado. At Glass Moon we seek to provide a unique, comfortable space where everyone feels welcomed. We strive to design custom tattoos of the highest quality and provide clients with an experience they can enjoy for a lifetime. To inquire about booking an appointment please visit our Artist page. </p>
         </div>
 
         <div className= 'links'>
