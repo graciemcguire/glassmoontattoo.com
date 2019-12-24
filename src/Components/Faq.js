2b95b4e506b5f2e776012991310a279881364a70
github username <github email address>
2019-12-24 10:01:16 -0700

    faq-a

--- src/Components/Faq.js
+++ src/Components/Faq.js
@@ -30,7 +30,7 @@ export default class Faq extends Component {
               </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel className='accordion-panel' className='accordion-panel'>
-              <p>
+              <p className='faq-a'>
                 1151 s Huron st, Denver CO, 80223
               </p>
             </AccordionItemPanel>
@@ -43,7 +43,7 @@ export default class Faq extends Component {
                 </AccordionItemButton>
               </AccordionItemHeading>
               <AccordionItemPanel className='accordion-panel'>
-                <p>
+                <p className='faq-a'>
                   Our studio is appointment only and we do not take walk-ins, we do have flash events a few times a year where we offer discounted flash-tattoos on a first-come first-serve basis.
                 </p>
               </AccordionItemPanel>
@@ -56,7 +56,7 @@ export default class Faq extends Component {
                 </AccordionItemButton>
               </AccordionItemHeading>
               <AccordionItemPanel className='accordion-panel'>
-                <p>
+                <p className='faq-a'>
                 Because Glass Moon is an appointment only studio we do not have set hours; while our schedules are somewhat consistent they tend to vary a bit week to week and we cannot guarantee that an artist will be present in the shop if you do not have an appointment.
                 </p>
               </AccordionItemPanel>
@@ -69,7 +69,7 @@ export default class Faq extends Component {
                 </AccordionItemButton>
               </AccordionItemHeading>
               <AccordionItemPanel className='accordion-panel'>
-                <p>
+                <p className='faq-a'>
                   The cost of the tattoo will vary depending on your artist, the size of the tattoo, the placement on the body, and the complexity of the design.  For some projects artists will charge by the hour and for others they will provide a flat price.  Each artist sets their own prices and will happily discuss your budget and the expected price prior to the tattoo.
                 </p>
               </AccordionItemPanel>
@@ -82,7 +82,7 @@ export default class Faq extends Component {
                 </AccordionItemButton>
               </AccordionItemHeading>
               <AccordionItemPanel className='accordion-panel'>
-                <p>
+                <p className='faq-a'>
                   All tattoos require a non-refundable deposit, the deposit amount varies by artist.  We require 48-hour notice to reschedule and retain your deposit. Deposits go toward the final price of your tattoo.
                 </p>
               </AccordionItemPanel>
@@ -95,7 +95,7 @@ export default class Faq extends Component {
                 </AccordionItemButton>
               </AccordionItemHeading>
               <AccordionItemPanel className='accordion-panel'>
-                <p>
+                <p className='faq-a'>
                   Glass Moon is a strictly 18+ studio.  We do not tattoo anyone under the age of 18 with or without parental permission.  We do require ID prior to getting tattooed.
                 </p>
               </AccordionItemPanel>
@@ -108,7 +108,7 @@ export default class Faq extends Component {
                 </AccordionItemButton>
               </AccordionItemHeading>
               <AccordionItemPanel className='accordion-panel'>
-                <p>
+                <p className='faq-a'>
                   Our artists do all of their own booking, please visit our artist page to find out more about how each artist prefers to do their booking.
                 </p>
               </AccordionItemPanel>
@@ -121,7 +121,7 @@ export default class Faq extends Component {
                 </AccordionItemButton>
               </AccordionItemHeading>
               <AccordionItemPanel className='accordion-panel'>
-                <p>
+                <p className='faq-a'>
                   While we understand that getting a new tattoo is a very exciting experience we ask that you bring no more than one friend along with you to your appointment.  Our studio is quite small and we want to ensure everyone is comfortable.
                 </p>
               </AccordionItemPanel>
@@ -134,7 +134,7 @@ export default class Faq extends Component {
                 </AccordionItemButton>
               </AccordionItemHeading>
               <AccordionItemPanel className='accordion-panel'>
-                <p>
+                <p className='faq-a'>
                   We will generally only tattoo the neck, face, or hands of those who are heavily tattooed.  We take tattooing these highly visible places very seriously and want to ensure clients have a lot of experience being tattooed before committing to these parts of the body.
                 </p>
               </AccordionItemPanel>
@@ -147,7 +147,7 @@ export default class Faq extends Component {
                 </AccordionItemButton>
               </AccordionItemHeading>
               <AccordionItemPanel className='accordion-panel'>
-                <p>
+                <p className='faq-a'>
                   Our artists reserve the right to accept or refuse projects at their own discretion.  We encourage our artists to pursue projects that align with their artistic vision which means your project may not always be accepted.  If an artist declines your project we are happy to recommend another artist in town whose work more consistently aligns with your idea.
                 </p>
               </AccordionItemPanel>
@@ -160,7 +160,7 @@ export default class Faq extends Component {
                 </AccordionItemButton>
               </AccordionItemHeading>
               <AccordionItemPanel className='accordion-panel-healing'>
-              <ul>
+              <ul className='faq-a'>
                 <li>If you are healing your tattoo with second skin (tegaderm, saniderm) you can leave this bandage on for 3 to 4 days as long as it is fully sealed and has no leakage.  Fluid build up under the bandage is normal but if it leaks out of the bandage the bandage should be removed.</li>
 
                 <li>After the 3-4 days remove the second skin when you are in the shower, clean the tattoo with soapy water (dr. Bronner’s soap preferred) and let the tattoo dry completely.  Apply a layer of unscented lotion to tattoo and rub it all the way in.</li>
